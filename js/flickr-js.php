1d9ddecc25f914b420a4d7c047e8ea01d9e4a42b
tgardner <tgardner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-02-06 23:50:16 +0000

    git-svn-id: http://svn.wp-plugins.org/wordpress-flickr-manager/trunk@30961 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/flickr-js.php
+++ js/flickr-js.php
@@ -124,7 +124,7 @@ function insertImage(image,owner,id,name) {
 	}
 	var lightbox_size = document.getElementById("flickr-lbsize");
 	if(document.getElementById("flickr-lightbox").checked) {
-		imgHTML = '<a href="http://www.flickr.com/photos/' + owner + "/" + id + '/"' + target + 'title="' + image.alt + '"';
+		imgHTML = '<a href="http://www.flickr.com/photos/' + owner + "/" + id + '/"' + target + ' title="' + image.alt + '"';
 		imgHTML = imgHTML + ' rel="flickr-mgr"><img src="' + image.src + '" alt="' + image.alt + '" class="' + lightbox_size.options[lightbox_size.selectedIndex].value + '" /></a>';
 	} else {
 		imgHTML = '<a href="http://www.flickr.com/photos/' + owner + "/" + id + '/" title="' + image.alt + '"' + target + 'title="' + image.alt + '"' + '>';

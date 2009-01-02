a56ea09297185ff065a749696768e6dc4ff205ed
tgardner <tgardner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-01-02 02:56:34 +0000

    git-svn-id: http://svn.wp-plugins.org/wordpress-flickr-manager/trunk@83561 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- geturl.php
+++ geturl.php
@@ -2,14 +2,23 @@
 // don't cache this page
 header("Cache-Control: no-cache");
 function geturl($url){
-	$ch = curl_init();
-	$timeout = 5;
-	curl_setopt($ch, CURLOPT_URL, $url);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
-	// make this curl_exec, no spaces
-	$results = curl_exec($ch);
-	curl_close($ch);
+	if(function_exists('curl_init')) {
+		$ch = curl_init();
+		$timeout = 5;
+		curl_setopt($ch, CURLOPT_URL, $url);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+		// make this curl_exec, no spaces
+		$results = curl_exec($ch);
+		curl_close($ch);
+	} else {
+		$ch = fopen($url, "rb");
+		$results = '';
+		while (!feof($ch)) {
+			$results .= fread($ch, 1024);
+		}
+		fclose($ch);
+	}
 	return $results;
 }
 // get the specified url from the querystring

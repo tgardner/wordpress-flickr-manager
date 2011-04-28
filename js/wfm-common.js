2076e22c2c1750d2ebf26de892b0aa36d1907ae4
tgardner <tgardner@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-28 05:22:55 +0000

    git-svn-id: http://svn.wp-plugins.org/wordpress-flickr-manager/trunk@378211 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/wfm-common.js
@@ -0,0 +1,60 @@
+var wfmJS = jQuery.noConflict();
+
+String.prototype.format = function() {
+    var formatted = this;
+    for(arg in arguments) {
+        formatted = formatted.replace("{" + arg + "}", arguments[arg]);
+    }
+    return formatted;
+};
+
+function GetFlickrHref(link) 
+{
+	var image = wfmJS('img:first', link);
+	var href = '';
+	
+	if(image.hasClass('flickr-original') || image.hasClass('flickr-site_mp4') || image.hasClass('flickr-video_player')) {
+		href = image.attr('longdesc');
+	} else {
+		var imageSize = '';
+		
+		if(image.hasClass('flickr-large')) {
+			imageSize = '_b';
+		} else if(image.hasClass('flickr-small')) {
+			imageSize = '_m';
+		} else if(image.hasClass('flickr-medium_640')) {
+			imageSize = '_z'
+		} 
+		
+		href = image.attr('src').replace(/(_[stmzb])?\.jpg$/, '{0}.jpg'.format(imageSize));
+	}
+	
+	return href;
+}
+
+function GetTitle(link)
+{
+	var anchor = wfmJS(link);
+	var caption = anchor.attr('title');
+	if(WFM_CaptionLink == 'true') {
+			caption += ' <a href="{0}" title="{1}"><img src="{2}/images/flickr-media.gif" alt="{3}" /></a>'
+								.format(anchor.attr("href"), WFM_ViewOnFlickr, WFM_PluginDir, WFM_ViewOnFlickr);
+	}
+	
+	return caption;
+}
+
+function GetDescription(link) {
+	var image = wfmJS('img:first',link);
+	var title = image.attr('title');
+	var license = wfmJS('#license-' + GetPhotoID(image));
+	if(license.size() > 0) {
+		title +=  license.html();
+	}
+	
+	return title; 
+}
+
+function GetPhotoID(image) {
+	return /\/([^_\/]+)[^\/]*$/g.exec(wfmJS(image).attr('src'))[1];
+}
